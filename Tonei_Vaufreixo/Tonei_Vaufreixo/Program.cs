c942225709fd97aaa386f613eec0ab9af6e0c13d
Patrick Roberto <patricktrp02@gmail.com>
2019-03-06 09:37:07 -0300

    Firs
    Criação do pacote
    Classes bases
    Lista de tarefas está no Program.cs

---
+++ Tonei_Vaufreixo/Tonei_Vaufreixo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonei_Vaufreixo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar metodo de cabeçalho - 1
+
+            //Metodo - entrada - saida - 2
+            //Criar metodo de leitura do arquivo de entrada - caminho do arquivo de entrada - colocar a entrada em uma lista/vetor de objetos
+
+            //Criar de forma recursiva e interativa - 3
+                /* 
+                 * Criar metodo de calculo de gasto vencendo cada openente - lista/vetor com as entradas - retorna uma lista com as possiblidade de pontos; esforço; e relação de vitorias;
+                 * Criar um objeto que represente as possiblidades de saida
+                */
+
+                /*
+                 * criar metodo que selecina a melhor posição que o Sir Ducan obteve - Lista de possibilidades - melhor possibilidade
+                 * a regra selecionar a que teve o menor gasto, mas que mantenha dentro dos K conpetidores
+                */
+            //Exibir resultado - saido do metodo que seleciona o melhor competidor - exib o esforço minimo em tela. - 5
+
+        }
+    }
+}
